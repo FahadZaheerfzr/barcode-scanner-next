59b5db658fb68c576b1f818abd625cc80559c3de
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-03-19 19:09:03 +0500

    Found and in excel added

--- components/BarcodeScanner.js
+++ components/BarcodeScanner.js
@@ -127,7 +127,7 @@ export default function BarcodeScanner() {
                         <p className="uppercase text-lg lg:text-4xl">Please place the barcode in camera view</p>
                         <p className="uppercase text-base lg:text-4xl">Please keep the camera still and focus on the barcode</p>
                         <p className="uppercase text-base lg:text-4xl font-bold">Drop Number:</p>
-                        <p className="uppercase text-3xl lg:text-4xl font-bold">XX</p>
+                        <p className="uppercase text-3xl lg:text-4xl font-bold">{found? foundNumber : "XX"}</p>
                     </div>
             }
         </React.Fragment>
