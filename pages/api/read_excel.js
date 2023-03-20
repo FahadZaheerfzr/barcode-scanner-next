14adcbc862b43a6d48fcce4e7c3c5b79f9c6ce79
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-03-21 02:03:48 +0500

    Handled missing values

--- pages/api/read_excel.js
+++ pages/api/read_excel.js
@@ -9,14 +9,21 @@ export default async function handler(req, res) {
         const rows = await readXlsxFile(`./public/excels/${route}/data_${route}.xlsx`);
 
         for (let i = 0; i < rows.length; i++) {
-            data.push({
-                "STOP NUMBER": rows[i][0],
-                "CONSIGNMENT NUMBER": rows[i][1],
-            })
+            if (rows[i][0] === null) {
+                data.push({
+                    "STOP NUMBER": data[data.length - 1]["STOP NUMBER"],
+                    "CONSIGNMENT NUMBER": rows[i][1],
+                })
+            } else {
+                data.push({
+                    "STOP NUMBER": rows[i][0],
+                    "CONSIGNMENT NUMBER": rows[i][1],
+                })
+            }
         }
         res.status(200).json(data)
     } catch (e) {
         res.status(400).json("error")
     }
-    
+
 }
