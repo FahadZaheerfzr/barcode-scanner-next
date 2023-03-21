8b6cef2e5e0c24d1cc3ed17ab9aed2daaaeb6a45
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-03-22 00:45:43 +0500

    region of interest updated

--- components/StrichScanner.js
+++ components/StrichScanner.js
@@ -41,7 +41,7 @@ export default function StrichBarcodeScanner({setData}) {
             vibration: true
           },
           locator: {
-            regionOfInterest: { left: 0.1, right: 0.1, top: 0.6, bottom: 0.6 }
+            regionOfInterest: { left: 0.1, right: 0.1, top: 0.1, bottom: 0.1 }
           }
         };
         console.log("here1");
